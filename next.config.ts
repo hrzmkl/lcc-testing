28e86bb75ee71920ee7e5a3229493c1087699d64
mickaelrmt <mickael@rmt-itsolutions.com>
2025-06-27 14:57:45 +0300

    debug: deploy error on testing

--- next.config.ts
+++ next.config.ts
@@ -70,8 +70,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-module.exports = withBundleAnalyzer(nextConfig)
+
+//module.exports = withBundleAnalyzer(nextConfig)
